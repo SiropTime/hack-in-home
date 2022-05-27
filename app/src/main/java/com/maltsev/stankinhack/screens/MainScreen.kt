808bf4768c08c4af68bcbe59eb47d24a5ae5bdec
SiropTime <sirop4beats@gmail.com>
2022-05-27 23:05:31 +0300

    UI отправки сообщений

--- app/src/main/java/com/maltsev/stankinhack/screens/MainScreen.kt
+++ app/src/main/java/com/maltsev/stankinhack/screens/MainScreen.kt
@@ -1,12 +1,21 @@
 package com.maltsev.stankinhack.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mic
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,6 +30,14 @@ fun MainScreenPreview() {
 
 @Composable
 fun MainScreen(navController: NavController) {
+    val messageFieldText = remember {
+        mutableStateOf("")
+    }
+
+    val canTalking = remember {
+        mutableStateOf(true)
+    }
+
     Surface {
 
         Column(
@@ -31,12 +48,69 @@ fun MainScreen(navController: NavController) {
             verticalArrangement = Arrangement.SpaceBetween
 
         ) {
+
+            // Row with message field, voice button and send button
             Row(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .padding(bottom = 10.dp),
                 horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
                     ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .height(50.dp)
+                        .clip(CircleShape)
+                        .border(
+                            color = MaterialTheme.colors.primary,
+                            width = 2.dp,
+                            shape = CircleShape
+                        ),
+                    value = messageFieldText.value,
+                    onValueChange = {
+                        canTalking.value = false
+                        messageFieldText.value = it
+                    })
 
+                if (canTalking.value) {
+                    // Voice button
+                    OutlinedButton(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .height(50.dp)
+                            .border(
+                                color = MaterialTheme.colors.primary,
+                                width = 2.dp,
+                                shape = CircleShape
+                            ),
+                        shape = CircleShape,
+                        ) {
+                        Icon(Icons.Outlined.Mic, "Talk")
+                    }
+                } else {
+                    // Send Button
+                    OutlinedButton(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .height(50.dp)
+                            .border(
+                                color = MaterialTheme.colors.primary,
+                                width = 2.dp,
+                                shape = CircleShape
+                            ),
+                        shape = CircleShape,
+                        onClick = {
+                            canTalking.value = true
+                            messageFieldText.value = ""
+                        }
+                    ) {
+                        Icon(Icons.Outlined.Send, "Send")
+                    }
+                }
             }
         }
     }
