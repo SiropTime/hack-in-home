ba0f1d7127a1cea95396551ad0658371e8a52bc9
TheGoodie <kaka.kalash@mail.ru>
2022-05-29 06:48:48 +0300

    cors policy fix

--- src/main/java/com/degenerator/hackinhome/controller/requisition/RequisitionController.java
+++ src/main/java/com/degenerator/hackinhome/controller/requisition/RequisitionController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/requisition")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RequisitionController {
 
     @Autowired
--- src/main/java/com/degenerator/hackinhome/controller/student/StudentController.java
+++ src/main/java/com/degenerator/hackinhome/controller/student/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class StudentController {
 
     @Autowired
